b238521415c1a1a655dddcfe727c3a43637d10e8
Seungjae Park <astro.psj@gmail.com>
2018-12-28 23:40:12 +0900

    Add an animation on the button and remove scrollbar

--- page/main.js
+++ page/main.js
@@ -1,1 +1,10 @@
 import './particles';
+
+window.addEventListener('load', () => {
+  document.querySelector('#scroll-next').addEventListener('click', () => {
+    document.querySelector('main').scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+});
