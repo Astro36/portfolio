c2de351a8c73cd6b1dfc91e2ead8791384006d0f
Seungjae Park <astro.psj@gmail.com>
2020-10-04 20:58:49 +0900

    Fix webmanifest src url bug

--- postbuild.js
+++ postbuild.js
@@ -5,9 +5,9 @@ const fsPromises = fs.promises;
 
 (async () => {
   const distDir = path.join(__dirname, 'dist');
-  const entryFile = path.join(distDir, 'index.html');
+  const htmlFile = path.join(distDir, 'index.html');
 
-  let html = await fsPromises.readFile(entryFile);
+  let html = await fsPromises.readFile(htmlFile);
   html = html.toString();
 
   // Lazy Loading
@@ -18,5 +18,15 @@ const fsPromises = fs.promises;
   html = html.replace(/<time datetime="now"><\/time>/g, `<time datetime="${now}">${now}</time>`);
   html = html.replace(/datetime="now"/g, `datetime="${now}"`);
 
-  await fsPromises.writeFile(entryFile, html);
+  await fsPromises.writeFile(htmlFile, html);
+
+  const manifestFile = path.join(distDir, 'manifest.webmanifest');
+  const manifest = JSON.parse(await fsPromises.readFile(manifestFile));
+
+  const distFiles = await fsPromises.readdir(distDir);
+  manifest.icons.forEach((icon) => {
+    icon.src = distFiles.find((file) => file.startsWith(path.parse(icon.src).name));
+  });
+
+  await fsPromises.writeFile(manifestFile, JSON.stringify(manifest));
 })();
